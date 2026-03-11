f5371cbdf34433404c272de6166baa852b6c80d7
feva0001-boop <feva0001@stud.ek.dk>
2026-03-11 19:02:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,8 @@ def about():
 def cv():
     return render_template("cv.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
